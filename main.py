32235dc76c04fceaaa166b742f0e6f875dea7f4e
Max <testspb777@gmail.com>
2025-01-31 16:05:18 +0300

    add req.txt

---
+++ main.py
@@ -0,0 +1,12 @@
+from json.decoder import JSONDecodeError
+import requests
+
+response = requests.get("https://playground.learnqa.ru/api/get_text")
+
+print(response.text)
+
+try:
+    parsed_response_text = response.json() # если первая строка отработает ок, переходим к 2й, если ошибка идем в блок exept
+    print(parsed_response_text)
+except JSONDecodeError:
+    print("^^^ - это не JSON формат")

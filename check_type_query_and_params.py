f12ddaf8bd1dd9f52f60f39baa728ab5e356e987
Max <testspb777@gmail.com>
2025-02-01 13:02:04 +0300

    refactor check_type_query_and_params.py

--- check_type_query_and_params.py
+++ check_type_query_and_params.py
@@ -3,7 +3,10 @@ import requests
 response = requests.get("https://playground.learnqa.ru/api/check_type", params={"param_1": "value_1"})
 print(response)
 print(response.text)
+print(response.headers)
+print(response.content)
 
+print('***' * 5)
 response = requests.post("https://playground.learnqa.ru/api/check_type", data={"param_1": "value_1"})
 print(response)
 print(response.text)

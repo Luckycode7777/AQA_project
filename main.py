8f237c93f4b8587a4281373528bcc373662d702f
Max <testspb777@gmail.com>
2025-02-02 18:04:13 +0300

    files moved to another folders

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from json.decoder import JSONDecodeError
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/get_text")
+response = requests.get("https://api.chucknorris.io/jokes/random")
 
+url = 'https://api.chucknorris.io/jokes/random'
+print(url)
 print(response.text)
+
 
 try:
     parsed_response_text = response.json() # если первая строка отработает ок, переходим к 2й, если ошибка идем в блок exept

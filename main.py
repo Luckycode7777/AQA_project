c708516f0d739e491a487601cea4644b584a6989
Max <testspb777@gmail.com>
2025-02-08 13:36:53 +0300

    refactoring file main.py

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ response = requests.get("https://api.chucknorris.io/jokes/random")
 
 url = 'https://api.chucknorris.io/jokes/random'
 print(url)
-print(response.text)
+print(f'response.text >>> {response.text}')
 
 
 try:
     parsed_response_text = response.json() # если первая строка отработает ок, переходим к 2й, если ошибка идем в блок exept
-    print(parsed_response_text)
+    print(f'parsed_response_text >>> {parsed_response_text}')
 except JSONDecodeError:
     print("^^^ - это не JSON формат")

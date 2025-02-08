e0763d1dce52c88a8c47b9a758dfc51427c2246b
Max <testspb777@gmail.com>
2025-02-08 13:36:30 +0300

    refactoring file test_1.py

--- d_3_pytest/test_1.py
+++ d_3_pytest/test_1.py
@@ -3,7 +3,7 @@ import requests
 
 class TestFirstAPI:
     def test_hello(self):
-        url = "https://playground.learnqa.ru/api/hello"
+        url = "https://playground.learnqa.ru/api/hellos"
         name = "Max"
         data = {"name": name}
 
@@ -11,11 +11,13 @@ class TestFirstAPI:
 
         assert response.status_code == 200, 'Неверный код ответа/Wrong response code'
 
+        # ответ в json парсим в переменную response_dict
         response_dict = response.json()
         assert "answer" in response_dict, "Не найдено 'answer' в ответе"
+        print(f'dict(response.json()) >>> {dict(response.json())}')
 
         expected_response_text = f"Hello, {name}"
         actual_response_text = response_dict["answer"]
         assert actual_response_text == expected_response_text, "Актуальный ответ не корректынй"
-
-
+        print(f'actual_response_text >>> {actual_response_text}')
+        print(f'expected_response_text >>> {expected_response_text}')
